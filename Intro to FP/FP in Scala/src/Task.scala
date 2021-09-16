476fe99a3e072fb683e4471b5e2e12d7d6df03de
Joseph Bakouny <Youssef.Bakouny@usj.edu.lb>
2021-09-16 15:18:01 +0300

    Resolve MapReduce Exercice

--- Intro to FP/FP in Scala/src/Task.scala
+++ Intro to FP/FP in Scala/src/Task.scala
@@ -1,7 +1,15 @@
+import scala.annotation.tailrec
+
 object Task {
-  def sum (f : Int => Int, a : Int, b : Int): Int = {
-    if (a > b) 0 else f(a) + sum(f, a + 1, b)
-  }
+
+  def mapReduce(zero : Int, op : (Int, Int) => Int, f : Int => Int, a : Int, b : Int): Int =
+    if (a > b) zero else op(f(a), mapReduce(zero, op, f, a + 1, b))
+
+  def product(f : Int => Int, a : Int, b : Int): Int =
+    mapReduce(1, (x, y) => x * y, f, a, b)
+
+  def sum (f : Int => Int, a : Int, b : Int): Int =
+    mapReduce(0, (x, y) => x + y, f, a, b)
 
   def sumInt(a : Int, b : Int) : Int =
     sum(x => x, a, b)
@@ -9,8 +17,8 @@ object Task {
   def sumCube(a : Int, b : Int) =
     sum(x => x * x * x, a, b)
 
-  def fact(n : Int ): Int =
-    if (n == 0) 1 else n * fact(n - 1)
+  def fact(n : Int): Int =
+    product(x => x, 1, n)
 
   def sumFact(a : Int, b : Int) =
     sum(fact, a, b)
--- Intro to FP/FP in Scala/test/TestSpec.scala
+++ Intro to FP/FP in Scala/test/TestSpec.scala
@@ -4,12 +4,20 @@ import Task._
 
 class TestSpec extends FunSuite {
 
-  test("Testing the sumInts function") {
+  test("Testing the sumInt function") {
     assert(sumInt(5, 10) == 45, "SumInt impl is incorrect")
+  }
+
+  test("Testing the sumInt function for big numbers") {
+    assert(sumInt(5, 100000) == 705082694, "SumInt impl is incorrect")
   }
 
   test("Testing the sumCube function") {
     assert(sumCube(5, 10) == 2925, "sumCube impl is incorrect")
+  }
+
+  test("Testing the fact function") {
+    assert(fact(5) == 120, "fact impl is incorrect")
   }
 
   test("Testing the sumFact function") {
